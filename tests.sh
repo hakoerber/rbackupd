4a54d9286a31cb3d49e61b2413444d6fbd86927c
Hannes Körber <hannes.koerber@gmail.com>
2013-10-09 04:24:55 +0200

    Adds pep8 to the tests, tests fail if pep8 complains.

--- tests.sh
+++ tests.sh
@@ -1,5 +1,8 @@
+#!/usr/bin/env bash
 # This script runs all tests in ./tests, and adds ./src to PYTHONPATH
 # before doing so.
+
+retval=0
 
 ROOTDIR="$(dirname $0)"
 TESTDIR="tests"
@@ -12,3 +15,11 @@ python -m unittest discover \
 --start-directory "$ROOTDIR/$TESTDIR" \
 --pattern "$TESTPATTERN" \
 --top-level-directory "$ROOTDIR"
+
+retval=$?
+
+pep8 "$ROOTDIR/$PKGSDIR" --filename="*.py" --count
+[[ $? != 0 ]] && retval=10
+
+[[ $retval == 0 ]] && echo "All tests passed." || echo "Tests failed."
+exit $retval
