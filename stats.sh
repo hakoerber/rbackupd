777815d94dbc67b9daa33dde74cc1060deac9852
Hannes Körber <hannes.koerber@gmail.com>
2013-11-08 17:18:08 +0100

    Fix stats.sh as source directory changed.

--- stats.sh
+++ stats.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-all_lines="$(cat $(find src/ -name '*.py'))"
-wc -l $(find src/ -name "*.py") | sort -gr
+all_lines="$(cat $(find rbackupd/ -name '*.py'))"
+wc -l $(find rbackupd/ -name "*.py") | sort -gr
 echo
-echo "SLOC:            $(cat $(find src/ -name '*.py') | grep -v '^[[:space:]]*$' | grep -v '^[[:space:]]*#.*$' | wc -l)"
-echo "Blank lines:     $(cat $(find src/ -name '*.py') | grep '^[[:space:]]*$' | wc -l)"
-echo "Comment lines:   $(cat $(find src/ -name '*.py') | grep '^[[:space:]]*#.*$' | wc -l)"
-echo "Lines overall:   $(cat $(find src/ -name '*.py') | wc -l)"
+echo "SLOC:            $(cat $(find rbackupd/ -name '*.py') | grep -v '^[[:space:]]*$' | grep -v '^[[:space:]]*#.*$' | wc -l)"
+echo "Blank lines:     $(cat $(find rbackupd/ -name '*.py') | grep '^[[:space:]]*$' | wc -l)"
+echo "Comment lines:   $(cat $(find rbackupd/ -name '*.py') | grep '^[[:space:]]*#.*$' | wc -l)"
+echo "Lines overall:   $(cat $(find rbackupd/ -name '*.py') | wc -l)"
