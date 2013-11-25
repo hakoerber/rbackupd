ef158dd8fc9c414e9dee84b4782adeedae613002
Hannes Körber <hannes.koerber@gmail.com>
2013-11-25 16:16:04 +0100

    Make tests pep8-compliant.

--- tests/test_cron.py
+++ tests/test_cron.py
@@ -22,6 +22,7 @@ import itertools
 
 import cron
 
+
 class Tests(unittest.TestCase):
 
     def setUp(self):
@@ -31,49 +32,46 @@ class Tests(unittest.TestCase):
         self.now = datetime.datetime.now()
 
         # higest possible value
-        self.d_in_hi    =  datetime.datetime(2015, 11,  5, 15,  1)
+        self.d_in_hi = datetime.datetime(2015, 11,  5, 15,  1)
         # lowest possible value
-        self.d_in_lo    =  datetime.datetime(2012,  6,  5, 10,  1)
+        self.d_in_lo = datetime.datetime(2012,  6,  5, 10,  1)
 
         # datetimes maching an occurence exactly
         self.d_in_exact = [self.d_in_hi, self.d_in_lo,
                            datetime.datetime(2013, 8, 5, 13, 1)]
 
-
         # datetimes older than the lowest possible value
-        self.d_out_lo   = [datetime.datetime(2011,  6,  5, 10,  1),
-                           datetime.datetime(2012,  5,  5, 10,  1),
-                           datetime.datetime(2012,  6,  4, 10,  1),
-                           datetime.datetime(2012,  6,  5,  9,  1),
-                           datetime.datetime(2012,  6,  5, 10,  0)]
-
+        self.d_out_lo = [datetime.datetime(2011,  6,  5, 10,  1),
+                         datetime.datetime(2012,  5,  5, 10,  1),
+                         datetime.datetime(2012,  6,  4, 10,  1),
+                         datetime.datetime(2012,  6,  5,  9,  1),
+                         datetime.datetime(2012,  6,  5, 10,  0)]
 
         # datetimes younger than the lowerst possible value
-        self.d_out_hi   = [datetime.datetime(2016, 11,  5, 15,  1),
-                           datetime.datetime(2015, 12,  5, 15,  1),
-                           datetime.datetime(2015, 11,  6, 15,  1),
-                           datetime.datetime(2015, 11,  5, 16,  1),
-                           datetime.datetime(2015, 11,  5, 15,  2)]
+        self.d_out_hi = [datetime.datetime(2016, 11,  5, 15,  1),
+                         datetime.datetime(2015, 12,  5, 15,  1),
+                         datetime.datetime(2015, 11,  6, 15,  1),
+                         datetime.datetime(2015, 11,  5, 16,  1),
+                         datetime.datetime(2015, 11,  5, 15,  2)]
 
         # datetimes younger or equal than the oldest and older or equal than
         # the yougest possible value
-        self.d_in_any   = [datetime.datetime(2013,  8,  5, 13,  1),
-                           datetime.datetime(2014,  3,  7, 23, 12),
-                           datetime.datetime(2014,  1,  5, 15,  1)] + \
-                          [self.d_in_lo, self.d_in_hi]
-
+        self.d_in_any = [datetime.datetime(2013,  8,  5, 13,  1),
+                         datetime.datetime(2014,  3,  7, 23, 12),
+                         datetime.datetime(2014,  1,  5, 15,  1)] + \
+                        [self.d_in_lo, self.d_in_hi]
 
         # datetimes older than the oldest possible value and younger than the
         # youngest possible value
-        self.d_out_any  = [datetime.datetime(1999, 8, 5, 13, 1),
-                           datetime.datetime(2023, 5, 24, 2, 54)] + \
-                          self.d_out_hi + \
-                          self.d_out_lo
+        self.d_out_any = \
+            [datetime.datetime(1999, 8, 5, 13, 1),
+             datetime.datetime(2023, 5, 24, 2, 54)] + \
+            self.d_out_hi + \
+            self.d_out_lo
 
-        self.d_all      = self.d_in_exact + self.d_out_lo + self.d_out_hi + \
-                          self.d_in_any + self.d_out_any + [self.now]
-
-
+        self.d_all = \
+            self.d_in_exact + self.d_out_lo + self.d_out_hi + \
+            self.d_in_any + self.d_out_any + [self.now]
 
     def test_matching(self):
         for d in self.d_in_exact:
@@ -81,7 +79,6 @@ class Tests(unittest.TestCase):
 
         for d in self.d_out_any:
             self.assertFalse(self.c1.matches(d))
-
 
     def test_latest_occurence(self):
 
@@ -99,7 +96,8 @@ class Tests(unittest.TestCase):
 
     def test_latest_occurence_max(self):
         for d in self.d_out_hi:
-            self.assertEqual(self.c1.get_most_recent_occurence(d), self.d_in_hi)
+            self.assertEqual(self.c1.get_most_recent_occurence(d),
+                             self.d_in_hi)
 
     def test_max_time(self):
         self.assertEqual(self.c1.get_max_time(), self.d_in_hi)
