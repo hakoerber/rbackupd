b29d4ba3255ab9650a37027b639eb4e49199b3ce
Hannes Körber <hannes.koerber@gmail.com>
2014-06-21 01:03:06 +0200

    Allow custom reference dates and fix interval calculation.

--- rbackupd/schedule/interval.py
+++ rbackupd/schedule/interval.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 # Copyright (c) 2013 Hannes Körber <hannes.koerber+rbackupd@gmail.com>
 
+import copy
 import datetime
 import logging
 import sys
@@ -40,17 +41,24 @@ class Interval(object):
     def __init__(self, interval_string):
         self.interval_string = interval_string
 
-    def get_oldest_datetime(self):
+    def get_oldest_datetime(self, reference=None):
         """
         Returns the oldest datetime that still lies inside the interval.
 
+        :param reference: The reference time for the comparison. If missing, the
+            current date is taken.
+        :type reference: datetime instance
+
         :rtype: datetime instance
         """
-        return _interval_to_oldest_datetime(self.interval_string)
+        if reference is None:
+            reference = datetime.datetime.now().replace(microsecond=0)
+
+        return _interval_to_oldest_datetime(self.interval_string, reference)
 
 
-def _interval_to_oldest_datetime(interval):
-    result = datetime.datetime.now()
+def _interval_to_oldest_datetime(interval, reference):
+    result = copy.copy(reference)
     suffix = interval[-1:]
     value = int(interval[:-1])
     if suffix == "m":
@@ -62,20 +70,17 @@ def _interval_to_oldest_datetime(interval):
     elif suffix == "d":
         result = result - datetime.timedelta(days=value)
     elif suffix == "M":
-
-        year = (datetime.date.today().year +
-                (datetime.date.today().month - value - 1) // 12)
-        month = datetime.date.today().month - value % 12
-        if month == 0:
-            month = 12
+        year = (reference.year +
+                ((reference.month - 1) - value) // 12)
+        month = ((reference.month - 1) - value) % 12 + 1
         # get the last day of the month by going back one month from the first
         # day of the following month
-        month_plus_one = (month + 1) % 12
+        month_plus_one = ((month - 1) + 1) % 12 + 1
         last_day_of_month = (datetime.date(year=year,
                                            month=month_plus_one,
                                            day=1) -
                              datetime.timedelta(days=1)).day
-        day = datetime.date.today().day
+        day = reference.day
         if day > last_day_of_month:
             day = last_day_of_month
         result = result.replace(year=year, month=month, day=day)
